d1ac1d934f35ff09e0b3f6bcbdbc5adf45aca729
안정태 <ajt@anjeongtaeui-MacBookPro.local>
2021-06-11 13:39:18 +0900

    [Server]feat/updateuser add update.js

--- controllers/user/index.js
+++ controllers/user/index.js
@@ -2,5 +2,6 @@ module.exports = {
   signin: require('./signin'),
   signout: require('./signout'),
   signup: require('./signup'),
+  update: require('./update'),
   userinfo: require('./userinfo'),
 };
