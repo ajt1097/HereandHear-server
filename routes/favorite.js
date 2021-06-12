47b778201b87b713d526c749d76378e1337cba4c
banseokSuh <still3028@gmail.com>
2021-06-12 14:12:37 +0900

    [server] modify: feat/deleteFavorite addOrDelete.js

--- routes/favorite.js
+++ routes/favorite.js
@@ -3,8 +3,7 @@ var router = express.Router();
 
 const { favoriteController } = require('../controllers');
 
-router.post('/add', favoriteController.add.post);
-router.post('/delete', favoriteController.delete.post);
+router.post('/addOrDelete', favoriteController.addOrDelete.post);
 router.get('/list', favoriteController.list.get);
 
 module.exports = router;
