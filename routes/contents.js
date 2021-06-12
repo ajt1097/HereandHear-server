fb7181ac161d3e340fea38d4c3ed4d74f85a2923
안정태 <ajt@anjeongtaeui-MacBookPro.local>
2021-06-12 16:28:52 +0900

    [Server] add feat/recommend recommend.js

--- routes/contents.js
+++ routes/contents.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const { contentsController } = require('../controllers');
 
 router.post('/category', contentsController.category.post);
+router.post('/recommend', contentsController.recommend.post);
 router.get('/:id', contentsController.detail.get);
 
 module.exports = router;
