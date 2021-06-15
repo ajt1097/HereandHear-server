7304c0bbff4d3fc2d0964638f6061c768c52610b
banseokSuh <still3028@gmail.com>
2021-06-15 15:18:47 +0900

    [Server] feat/recommend edit: recommend.js

--- controllers/contents/recommend.js
+++ controllers/contents/recommend.js
@@ -3,21 +3,21 @@ const { content } = require("../../models");
 
 module.exports = {
   post: async (req, res) => {
-    let { weather, month, location } = req.body
+    let { weather, month } = req.body
 
     let foundData;
 
-    if (!location) {
+    if (weather) {
       foundData = await content.findAll({
         where: {
-          [Op.or]: [{ weather }, { month }],
+          weather: weather
         },
         // plain: true
       }).catch(err => res.json(err))
-    } else {
+    } else if (month) {
       foundData = await content.findAll({
         where: {
-          [Op.or]: [{ weather }, { month }, { location }],
+          month: month
         },
       }).catch(err => res.json(err))
     }
@@ -29,31 +29,35 @@ module.exports = {
     // 데이터 깔끔하게
     let arrangedData = foundData.map(el => el.get({ plain: true }))
     
-    // 계절, 월, 위치 데이터 배열
-    let weatherResults = [];
-    let monthResults = [];
-    let locationResults = [];
+    let result = arrangedData[Math.floor(Math.random() * arrangedData.length)]
+
+
+
+    // // 계절, 월, 위치 데이터 배열
+    // let weatherResults = [];
+    // let monthResults = [];
+    // let locationResults = [];
     
-    // 각 배열에 할당
-    for (let el of arrangedData) {
-      if (el.weather === weather) {
-        weatherResults.push(el)
-      }
-      if (Number(el.month) === month) {
-        monthResults.push(el);
-      }
-      if (el.location === location) {
-        locationResults.push(el);
-      }
-    }
+    // // 각 배열에 할당
+    // for (let el of arrangedData) {
+    //   if (el.weather === weather) {
+    //     weatherResults.push(el)
+    //   }
+    //   if (Number(el.month) === month) {
+    //     monthResults.push(el);
+    //   }
+    //   if (el.location === location) {
+    //     locationResults.push(el);
+    //   }
+    // }
 
-    // 각 배열에서 랜덤한 값 추출
-    let weatherData = weatherResults[Math.floor(Math.random() * weatherResults.length)]
-    let monthData = monthResults[Math.floor(Math.random() * monthResults.length)]
-    let locationData = locationResults[Math.floor(Math.random() * locationResults.length)]
+    // // 각 배열에서 랜덤한 값 추출
+    // let weatherData = weatherResults[Math.floor(Math.random() * weatherResults.length)]
+    // let monthData = monthResults[Math.floor(Math.random() * monthResults.length)]
+    // let locationData = locationResults[Math.floor(Math.random() * locationResults.length)]
 
-    // [날씨, 월, 위치]
-    let result = [weatherData, monthData, locationData];
+    // // [날씨, 월, 위치]
+    // let result = [weatherData, monthData, locationData];
 
     res.status(200).send(result);
   }
